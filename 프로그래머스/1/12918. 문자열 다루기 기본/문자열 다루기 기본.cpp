a2867093d7567d837696dfe147ae917819c2afcb
KIM HAN EUN <123617780+Kimhaneun@users.noreply.github.com>
2023-11-07 19:15:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+	bool answer = true;
+
+	if (s.size() == 4 || s.size() == 6)
+	{
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (!isdigit(s[i]))
+			{
+				answer = false;
+				break;
+			}
+		}
+	}
+	else
+	{
+		answer = false;
+	}
+	return answer;
+}
